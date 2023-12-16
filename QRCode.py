fdaf945d94daaaadb50bcd8f694d8d291306163b
Kunal Kawate <114325267+Kunal-kawate@users.noreply.github.com>
2023-12-16 18:12:38 +0530

    Add files via upload

---
+++ QRCode.py
@@ -0,0 +1,6 @@
+import  qrcode #qrcode python module
+
+png =   qrcode.make('https://instagram.com/kunya.thing') #enter your url here 'https://instagram.com/kunya.thing'
+
+png.save('KunyaThing.png') #write here your desired file name at the place of 'KunyaThing.png'
+
